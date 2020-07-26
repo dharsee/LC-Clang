5bf33a1cc0a4980ee3723f71d63b3df008a4b545
dharsee <67437710+dharsee@users.noreply.github.com>
2020-07-25 17:21:35 -0700

    Update findMinInRotatedSortedArray.cpp
    Adding recursive solution in addition to iterative solution.

--- findMinInRotatedSortedArray.cpp
+++ findMinInRotatedSortedArray.cpp
@@ -22,6 +22,41 @@ Output: 0
 
 class Solution {
 public:
+    
+    // Recursive approach
+    int bin_search(vector<int> a, int l, int h)
+    {
+        int mid = l + (h - l)/2;
+        
+        if (a[l] < a[mid]) return bin_search(a, mid, h);
+        else if (a[mid] < a[h]) return bin_search(a, l, mid);
+        else
+        {
+            if (a[mid] > a[h]) return a[h];
+            else if(a[mid] < a[l]) return a[mid];
+            else return a[l];
+        }
+    }
+    
+    
+    int findMin(vector<int>& nums) {
+        int n = nums.size();
+        
+        //if (n == 0) return -1;
+        if (n == 1) return nums[0];
+        
+        if (nums[0] < nums[n-1]) return nums[0];
+        
+        return bin_search(nums, 0, n - 1);
+        
+        
+    }
+    
+    
+    #if 0
+    
+    // Iterartive approach
+    
     int binsearch(vector<int> a)
     {
         int l = 0, h = 0, mid = 0;
@@ -63,4 +98,5 @@ public:
         
         
     }
+    #endif
 };
