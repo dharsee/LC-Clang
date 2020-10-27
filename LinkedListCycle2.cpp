9b5e2e4ef7cabef5dc0bda199d8297a325e679d8
dharsee <67437710+dharsee@users.noreply.github.com>
2020-10-27 09:30:56 -0700

    Create LinkedListCycle2.cpp

---
+++ LinkedListCycle2.cpp
@@ -0,0 +1,101 @@
+/*
+
+Linked List Cycle II
+
+Solution
+Given a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. 
+Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+Notice that you should not modify the linked list.
+
+Follow up:
+
+Can you solve it using O(1) (i.e. constant) memory?
+
+ 
+
+Example 1:
+
+
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+Example 2:
+
+
+Input: head = [1,2], pos = 0
+Output: tail connects to node index 0
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+Example 3:
+
+
+Input: head = [1], pos = -1
+Output: no cycle
+Explanation: There is no cycle in the linked list.
+ 
+
+Constraints:
+
+The number of the nodes in the list is in the range [0, 104].
+-105 <= Node.val <= 105
+pos is -1 or a valid index in the linked-list.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        
+        if (head == NULL) return head;
+        
+        if (head->next == NULL) return NULL;
+        
+        ListNode *fast = NULL, *slow = NULL, *curr = NULL;
+        
+        fast = head->next;
+        slow = head;
+        
+        while (fast && fast->next)
+        {
+            fast = fast->next->next;
+            slow = slow->next;
+            
+            if (slow == fast)
+                break;   
+        }
+            
+        if (fast == NULL || fast->next == NULL) return NULL;
+        
+        fast = head;
+        slow = slow->next; // This is very important step to detect beginning of the cycle 
+        // Remember we reach here only when there is a cycle in the list
+        
+        while (fast != slow)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+        
+        return fast;
+        
+        /*
+     
+                    16 / 16 test cases passed.
+            Status: Accepted
+            Runtime: 4 ms
+            Memory Usage: 8.2 MB
+            Submitted: 0 minutes ago
+        */
+        
+    }
+};
